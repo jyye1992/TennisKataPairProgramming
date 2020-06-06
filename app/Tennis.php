547fb5b0976a4f4646f1cd3bbe1086c0ffd2eeb7
lucky <lucky76588@gmail.com>
2020-06-06 14:53:01 +0800

    refactor: score

--- app/Tennis.php
+++ app/Tennis.php
@@ -29,17 +29,9 @@ class Tennis
             return $this->lookUp[$this->firstPlayerPoint] . '-all';
         }
 
-        if ($this->firstPlayerPoint == 1 && $this->secondPlayerPoint == 1) {
-            return 'fifteen-all';
-        } elseif ($this->firstPlayerPoint == 2 && $this->secondPlayerPoint == 2) {
-            return 'thirty-all';
-        }
-
         if ($this->firstPlayerPoint > 0) {
             return $this->lookUp[$this->firstPlayerPoint] . '-love';
         }
-
-        return 'love-all';
     }
 
     public function firstPlayerWinPoint(int $point)
