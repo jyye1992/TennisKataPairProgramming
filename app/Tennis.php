b918247cfb8547e159fb46f05bce2251cdefae92
lucky <lucky76588@gmail.com>
2020-06-06 14:31:15 +0800

    feat: fifteen_all

--- app/Tennis.php
+++ app/Tennis.php
@@ -7,9 +7,17 @@ namespace App;
 class Tennis
 {
     private $firstPlayerPoint;
+    /**
+     * @var int
+     */
+    private $secondPlayerPoint;
 
     public function score()
     {
+        if ($this->firstPlayerPoint == 1 && $this->secondPlayerPoint == 1) {
+            return 'fifteen-all';
+        }
+
         if ($this->firstPlayerPoint == 1) {
             return 'fifteen-love';
         }
@@ -20,5 +28,10 @@ class Tennis
     public function firstPlayerWinPoint($point)
     {
         $this->firstPlayerPoint = $point;
+    }
+
+    public function secondPlayerWinPoint(int $point)
+    {
+        $this->secondPlayerPoint = $point;
     }
 }
