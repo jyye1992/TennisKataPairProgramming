45362d0056f4358da96eb4660f767b671ebc6827
YNCBearz <bear19930414@gmail.com>
2020-06-06 14:42:50 +0800

    refactor

--- app/Tennis.php
+++ app/Tennis.php
@@ -16,18 +16,20 @@ class Tennis
      */
     private $secondPlayerPoint = 0;
 
+    private $lookUp = [
+        1 => 'fifteen',
+        2 => 'thirty',
+    ];
+
     public function score()
     {
-        if ($this->firstPlayerPoint == 2) {
-            return 'thirty-love';
-        }
 
         if ($this->firstPlayerPoint == 1 && $this->secondPlayerPoint == 1) {
             return 'fifteen-all';
         }
 
-        if ($this->firstPlayerPoint == 1) {
-            return 'fifteen-love';
+        if ($this->firstPlayerPoint > 0) {
+            return $this->lookUp[$this->firstPlayerPoint] . '-love';
         }
 
         return 'love-all';
